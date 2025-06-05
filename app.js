26b750e71778291126f3ed1ca033886d473e1b4e
Augustus Mendy <Augustus.mendy@snhu.edu>
2025-06-05 19:52:17 -0400

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -4,25 +4,38 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Define routers
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index'); // API router
+
+var handlebars =  require('hbs');
+
+//Bring in the database
+require('./app_api/models/db');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
-var handlebars = require('hbs');
 
-//handlebars partial
+// register handlebars partials (https://www.npmjs.com/package/hbs)
 handlebars.registerPartials(__dirname+ '/app_server/views/partials');
 
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+// wire-up routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel',  travelRouter);
+app.use('/api', apiRouter); // Mount the API routes under the '/api' base path
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_server/Controllers/travel.js
+++ app_server/Controllers/travel.js
@@ -1,9 +1,34 @@
-var fs = require ('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8')); 
+const fetch = require('node-fetch'); // Ensure this is correctly imported for API fetching
+const { json } = require('stream/consumers');
 
-/* GET travel view */
-const travel = (req , res) => {
-    res.render('travel',{title : "Travlr Getaways"});
+/* Define API Endpoint and Options */
+const tripsEndpoint = 'http://localhost:3000/api/trips';
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+    },
+};
+//var fs = require('fs');
+//JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+const travel = async function (req , res) {
+    // console.log('TRAVEL CONTROLLER BEGIN');
+     await fetch(tripsEndpoint, options)
+    .then((res) => res.json())
+    .then((json) => { 
+        let message = null;
+        if (!(json instanceof Array)) {
+          message = "API lookup error";
+          json = [];
+        } else {
+            message = "No trips esist in our database!";
+        }
+        res.render('travel', {title: 'Travlr Getaways', trips: json, message});
+    })
+    .catch(err => res.status(500).send(e.message));
 };
 
 module.exports = {
